65d8a1be90c5e0d1f2af6fe8ce40d2365f2a0ccd
Clint <clintp1@gmail.com>
2014-12-08 12:30:35 +0000

    added 5 new responses to chatbot

--- chatbot.rb
+++ chatbot.rb
@@ -9,7 +9,12 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+          	  'I play (.*)' => 'I love %{c1} too', 
+          	  'hey' => 'hey you',
+          	  'hello' => 'oh hello there',
+          	  'welcome' => 'how nice of you',
+          	  'take care' => 'be safe my friend'}
 
 puts "Hello, what's your name?"
 name = gets.chomp
